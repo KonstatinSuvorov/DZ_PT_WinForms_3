19ba92b954df922cbe31c47da4df9b3c68f20f48
КОНСТАНТИН Суворов <kniaze84@gmail.com>
2023-04-19 23:17:09 +0300

    Добавьте файлы проекта.

---
+++ DZ_PT_WinForms_3_1/Form2.Designer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_PT_WinForms_3_1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+
+
+        #endregion
+        private void InitializeComponent()
+        {
+            this.btnSearchF2 = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.textBoxF2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearchF2
+            // 
+            this.btnSearchF2.Location = new System.Drawing.Point(197, 226);
+            this.btnSearchF2.Name = "btnSearchF2";
+            this.btnSearchF2.Size = new System.Drawing.Size(75, 23);
+            this.btnSearchF2.TabIndex = 1;
+            this.btnSearchF2.Text = "Поиск";
+            this.btnSearchF2.UseMnemonic = false;
+            this.btnSearchF2.UseVisualStyleBackColor = true;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(0, 0);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(284, 199);
+            this.listBox.TabIndex = 2;
+            // 
+            // textBoxF2
+            // 
+            this.textBoxF2.Location = new System.Drawing.Point(13, 228);
+            this.textBoxF2.Name = "textBoxF2";
+            this.textBoxF2.Size = new System.Drawing.Size(178, 20);
+            this.textBoxF2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Вводите маску поиска, например «*.doc».";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxF2);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.btnSearchF2);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Окно поиска";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        private System.Windows.Forms.Button btnSearchF2;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox textBoxF2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ DZ_PT_WinForms_3_3/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace DZ_PT_WinForms_3_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxReadFile = new System.Windows.Forms.TextBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxReadFile
+            // 
+            this.textBoxReadFile.Location = new System.Drawing.Point(0, -1);
+            this.textBoxReadFile.Multiline = true;
+            this.textBoxReadFile.Name = "textBoxReadFile";
+            this.textBoxReadFile.ReadOnly = true;
+            this.textBoxReadFile.Size = new System.Drawing.Size(550, 250);
+            this.textBoxReadFile.TabIndex = 0;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(0, 257);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(266, 42);
+            this.btnUpload.TabIndex = 1;
+            this.btnUpload.Text = "Загрузить файл";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Enabled = false;
+            this.btnEdit.Location = new System.Drawing.Point(284, 257);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(266, 42);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Редактировать файл";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 311);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.textBoxReadFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Текстовый редактор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxReadFile;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnEdit;
+    }
+}
